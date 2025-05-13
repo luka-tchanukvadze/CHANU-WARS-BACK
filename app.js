14da9a60984526879d326f8c21c948399f639d30
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-13 19:10:48 +0400

    implementing global error handling

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
 import express from "express";
 import randomInfoRoute from "./routes/randomInfoRoute.js";
 import cors from "cors";
-
+import AppError from "./utils/appError.js";
+import { globalErrorHandler } from "./controllers/errorController.js";
 const app = express();
 
 // middleware for parsing request body
@@ -16,5 +17,11 @@ app.get("/", (request, response) => {
 });
 
 app.use("/randomInfos", randomInfoRoute);
+
+app.all("*", (req, res, next) => {
+  next(new AppError(`Can't find ${req.originalUrl} on this server`, 404));
+});
+
+app.use(globalErrorHandler);
 
 export default app;
