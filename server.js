69b320540db4a901822d357b3b41b60b964b4747
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-17 20:26:29 +0400

    fixing unhandledrejection function

--- server.js
+++ server.js
@@ -2,18 +2,19 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import app from "./app.js";
 
+process.on("uncaughtException", (err) => {
+  console.log("UNCAUGHT EXCEPTION! shutting down...");
+  console.log(err.name, err.message);
+  process.exit();
+});
+
 dotenv.config({ path: "./config.env" });
 
 const mongoDBURL = process.env.MONGO_URL;
 
-mongoose
-  .connect(mongoDBURL)
-  .then(() => {
-    console.log("app connected to database");
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+mongoose.connect(mongoDBURL).then(() => {
+  console.log("app connected to database");
+});
 
 const port = process.env.PORT || 3000;
 
@@ -21,7 +22,7 @@ const server = app.listen(port, () => {
   console.log(`app is listening to port: ${port}`);
 });
 
-process.on("uncaughtException", (err) => {
+process.on("unhandledRejection", (err) => {
   console.log(err.name, err.message);
   console.log("UNHANDLED REJECTION! shutting down...");
   server.close(() => {
