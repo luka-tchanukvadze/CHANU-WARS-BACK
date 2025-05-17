fa2dc3f5e090c2b386e640a9131f30807fb5decd
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-17 20:18:32 +0400

    unhandled rejections

--- server.js
+++ server.js
@@ -16,6 +16,15 @@ mongoose
   });
 
 const port = process.env.PORT || 3000;
-app.listen(port, () => {
+
+const server = app.listen(port, () => {
   console.log(`app is listening to port: ${port}`);
+});
+
+process.on("uncaughtException", (err) => {
+  console.log(err.name, err.message);
+  console.log("UNHANDLED REJECTION! shutting down...");
+  server.close(() => {
+    process.exit(1);
+  });
 });
