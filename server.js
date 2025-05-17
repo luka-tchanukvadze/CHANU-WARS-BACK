98fff5e881b377c016ba0016a5fe36475ed7f2c1
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-17 20:27:26 +0400

    handling uncaughtExceptions

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ import app from "./app.js";
 process.on("uncaughtException", (err) => {
   console.log("UNCAUGHT EXCEPTION! shutting down...");
   console.log(err.name, err.message);
+
   process.exit();
 });
 
