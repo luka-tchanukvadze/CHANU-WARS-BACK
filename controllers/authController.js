b68dfce214e64ae4952c370bcb9fc5e5ce7088e4
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-29 15:19:42 +0400

    Sending JWT with cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -12,7 +12,18 @@ const signToken = (id) => {
 
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
-  console.log("Generated token:", token);
+
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES * 24 * 60 * 60 * 1000
+    ),
+
+    httpOnly: true,
+  };
+
+  if (process.env.NODE_ENV === "production") cookieOptions.secure = true;
+
+  res.cookie("jwt", token, cookieOptions);
 
   res.status(statusCode).json({
     status: "success",
