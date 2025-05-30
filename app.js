a055c4ae9c3ee45200ceebfdf5f46199dc7d8dde
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-05-30 20:44:30 +0400

    data size limit

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ const limiter = rateLimit({
 app.use(limiter);
 
 // middleware for parsing request body
-app.use(express.json());
+app.use(express.json({ limit: "10kb" }));
 
 // Data sanitization against NoSQL query injection
 app.use(mongoSanitize());
