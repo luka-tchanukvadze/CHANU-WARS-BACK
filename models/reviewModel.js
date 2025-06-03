db5d4a43d7b248a2d5818b33317cdbbfb27900b0
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-03 17:57:33 +0400

    creating review model

--- models/reviewModel.js
+++ models/reviewModel.js
@@ -1,5 +1,32 @@
 import mongoose from "mongoose";
 
-const reviewSchema = new mongoose.Schema();
+const reviewSchema = new mongoose.Schema(
+  {
+    review: {
+      type: String,
+      required: [true, "Review can not be empty!"],
+    },
+    rating: {
+      type: Number,
+      min: 1,
+      max: 5,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    user: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: "User",
+        required: [true, "Review must belong to a user"],
+      },
+    ],
+  },
+  {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true },
+  }
+);
 
 export const Review = mongoose.model("Review", reviewSchema);
