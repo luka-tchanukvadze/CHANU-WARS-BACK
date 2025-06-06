00d32d71dec8d68d11940a22e46098603159bc27
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-06 12:17:40 +0400

    adding handlerFactory file

---
+++ controllers/handlerFactory.js
@@ -0,0 +1,17 @@
+import { eachRight } from "lodash";
+import { catchAsync } from "../utils/catchAsync.js";
+import AppError from "./../utils/appError.js";
+
+export const deleteOne = (Model) =>
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndDelete(req.params.id);
+
+    if (!doc) {
+      return next(new AppError("No document found with that ID", 404));
+    }
+
+    res.status(204).json({
+      status: "success",
+      data: null,
+    });
+  });
