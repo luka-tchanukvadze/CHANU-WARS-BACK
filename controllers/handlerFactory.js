7193f4b606f5939cb8bc60441bb9d9cc6f75d323
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-07 10:19:43 +0400

    updateOne

--- controllers/handlerFactory.js
+++ controllers/handlerFactory.js
@@ -14,3 +14,23 @@ export const deleteOne = (Model) =>
       data: null,
     });
   });
+
+export const updateOne = (Model) => {
+  catchAsync(async (req, res, next) => {
+    const doc = await Model.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!doc) {
+      return next(new "No document found with that ID"(), 404);
+    }
+
+    res.status(200).json({
+      status: "success",
+      data: {
+        doc,
+      },
+    });
+  });
+};
