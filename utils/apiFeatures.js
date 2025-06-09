c1c2b98ad2cbb54822eb443cd507602c09b5d848
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-09 18:15:30 +0400

    finishing apiFeatures class

--- utils/apiFeatures.js
+++ utils/apiFeatures.js
@@ -1,4 +1,4 @@
-class APIFeatures {
+export default class APIFeatures {
   constructor(query, queryString) {
     this.query = query;
     this.queryString = queryString;
@@ -38,6 +38,16 @@ class APIFeatures {
     } else {
       this.query = this.query.select("-__v");
     }
+
+    return this;
+  }
+
+  paginate() {
+    const page = this.queryString.page * 1 || 1;
+    const limit = this.queryString.limit * 1 || 1;
+    const skip = (page - 1) * limit;
+
+    this.query = this.query.skip(skip).limit(limit);
 
     return this;
   }
