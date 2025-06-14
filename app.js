902cdefe7fb0dee75acf3af2b587f90254a1a6d4
luka-tchanukvadze <tchanukvadze.luka@gmail.com>
2025-06-14 14:47:51 +0400

    cleanup

--- app.js
+++ app.js
@@ -38,7 +38,7 @@ app.use(mongoSanitize());
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
-  console.log(req.cookies);
+  // console.log(req.cookies);
 
   next();
 });
